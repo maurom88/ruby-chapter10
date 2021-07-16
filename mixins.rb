b304efd3fe8f59989ded01371371f0ba197391f0
Mauro Meden <mauro.meden@shopify.com>
2021-07-16 14:00:57 -0600

    Add mixins file with 3 modules

---
+++ mixins.rb
@@ -0,0 +1,37 @@
+module Call
+    def call_emergency_number
+        puts "Calling 911"
+    end
+
+    def call_home
+        put "E.T. phone homme"
+    end
+end
+
+module Walk
+    def walk_to_work
+        puts "Walking to work"
+    end
+
+    def walk_home
+        puts "Walking home"
+    end
+
+    def walk_this_way
+        puts "Talk this way"
+    end    
+end
+
+module Actions
+    def work
+        puts "I'm working!"
+    end
+    
+    def take_a_break
+        puts "Taking a break!"
+    end
+
+    def eat_lunch
+        puts "Eating lunch!"
+    end
+end
