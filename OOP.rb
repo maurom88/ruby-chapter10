88edd2c849ffe7ff04b9d50fae03d2b468fcf6f2
Mauro Meden <mauro.meden@shopify.com>
2021-07-17 13:59:24 -0600

    Move functional code to main.rb from OOP.rb

--- OOP.rb
+++ OOP.rb
@@ -115,30 +115,3 @@ class Administrator < User
         p @rights
     end
 end
-
-puts " --- "
-
-admin = Administrator.new("admin@website.com", "adminpass")
-admin.login("admin@website.com", "adminpass")
-admin.print_rights
-admin.print_test_array
-admin.logout
-
-puts " --- "
-
-teacher = Teacher.new("teacher@website.com", "teacherpass")
-teacher.login("admin@website.com", "adminpass")
-teacher.login("teacher@website.com", "teacherpass")
-teacher.language = "English"
-teacher.language = "Spanish"
-teacher.print_languages
-teacher.print_test_array
-
-puts " --- "
-
-admin.remove_last_test_array_value(admin.email)
-teacher.remove_last_test_array_value(teacher.email)
-
-puts " --- "
-
-teacher.eat_lunch
